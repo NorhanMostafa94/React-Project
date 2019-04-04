af6301dbfa52070d55a4120a730dfaddf568c931
DoaaOsama <doaa_kandeel@hotmail.com>
2019-04-04 23:55:26 +0200

    updating warnings

--- src/Component/Authors/List.js
+++ src/Component/Authors/List.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
-import { Context } from '../../App';
-import { Container, Row } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Context } from "../../App";
+import { Container, Row } from "react-bootstrap";
 
-import Author from './Author'
-import Paging from '../shared/Pagination';
-import NavBar from '../shared/Navbar'
+import Author from "./Author";
+import Paging from "../shared/Pagination";
+import NavBar from "../shared/Navbar";
 
 // import Book from '../Books/BookItem'
 class AuthorsList extends Component {
+  render() {
+    return (
+      <Context.Consumer>
+        {value => (
+          <>
+            <NavBar />
+            {/* // this.get(value.getCategories) */}
+            <Container className="card-container">
+              <Row className="justify-content-md-center authors-list">
+                {value.state.authors.map(
+                  d => (
+                    <Author key={d.id} {...d} />
+                  )
+                  // <Book key={d.id} {...d}></Book>
+                )}
+              </Row>
+              <Paging />
+            </Container>
+          </>
+        )}
+      </Context.Consumer>
+    );
+  }
+}
 
-    render() {
-        return (
-            <Context.Consumer>
-                {
-                    value => (
-                        <>
-                        <NavBar/>
-                            {/* // this.get(value.getCategories) */}
-                            <Container className="card-container" >
-                                <Row className="justify-content-md-center" className="authors-list">
-                                    {value.state.authors.map(d =>
-                                        <Author key={d.id} {...d}></Author>
-                                        // <Book key={d.id} {...d}></Book>
-                                    )
-                                    }
-                                </Row>
-                                <Paging />
-                            </Container>
-                            </>
-                            )
-                        }
-            </Context.Consumer>
-                    )
-                }
-                }
-                
 export default AuthorsList;
